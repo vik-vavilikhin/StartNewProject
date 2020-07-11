531bec8d1265cc14a5c986989a1dabc8348d0f95
Viktor Vavilikhin <vva76@yandex.ru>
2020-07-11 10:36:17 +0300

    Update

--- webpack/#src/js/index.js
+++
@@ -1,8 +0,0 @@
-import './../css/style.css';
-
-const body = document.querySelector('body');
-const title = document.createElement('h1');
-title.classList.add('title');
-title.textContent = 'Header text';
-
-body.append(title);
