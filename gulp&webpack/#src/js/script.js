1d9f4130a2486b3ce0897e1ecbaff843c62cb7fa
Viktor Vavilikhin <vva76@yandex.ru>
2020-08-02 11:51:20 +0300

    Update

--- gulp&webpack/#src/js/script.js
+++ gulp&webpack/#src/js/script.js
@@ -1,6 +1,5 @@
 import * as app from './modules/app';
 import './../layout/s-header/burger';
-import './../layout/s-features/b-menu-features/menu-features';
 
 document.addEventListener('DOMContentLoaded', () => {
   'use strict';
